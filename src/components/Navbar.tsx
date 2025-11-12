3faf8d0cf1d95227e7ce72bb89853085558b3368
Kate Hamada <kateh2@hawaii.edu>
2025-11-12 12:07:59 -1000

    1 edit navbar
    changed:
    - seed.ts and settings.development.json file to make an admin account
    - added temporary logo
    - changed navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -14,8 +14,8 @@ const NavBar: React.FC = () => {
   const { data: session } = useSession();
   const pathname = usePathname();
   const currentUser = session?.user?.email;
-  // const userWithRole = session?.user as { email: string; randomKey: string };
-  // const role = userWithRole?.randomKey;
+  const userWithRole = session?.user as { email: string; randomKey: string };
+  const role = userWithRole?.randomKey;
   const menuStyle = { marginBottom: '0px' };
   const navbarClassName = currentUser ? 'bg-dark' : 'bg-light';
   // const navbarVariant = currentUser ? 'dark' : 'light';
@@ -24,64 +24,52 @@ const NavBar: React.FC = () => {
       <Container>
         <Navbar.Brand href="/" className="align-items-center">
           <span style={{ fontWeight: 800, fontSize: '24px' }}>
-            <Image src="/images/logo.png" width={50} style={{ marginBottom: 3 }} alt="Bowfolios" />
-            Bowfolios
+            RIBows
+            <Image src="/images/temp.png" width={50} style={{ marginBottom: 3 }} alt="Rainbow" />
           </span>
         </Navbar.Brand>
         <Navbar.Toggle aria-controls={ComponentIDs.basicNavbarNav} />
         <Navbar.Collapse id={ComponentIDs.basicNavbarNav}>
           <Nav className="me-auto justify-content-start">
             {currentUser ? (
-              <Nav.Link id={ComponentIDs.homeMenuItem} active={pathname === '/home'} href="/home" key="home">
-                Home
+              <Nav.Link
+                id="search-nav"
+                active={pathname === '/search'}
+                href="/search"
+                key="search"
+              >
+                Search
               </Nav.Link>
             ) : (
               ''
             )}
-            <Nav.Link
-              id={ComponentIDs.profilesMenuItem}
-              active={pathname === '/profiles'}
-              href="/profiles"
-              key="profiles"
-            >
-              Profiles
-            </Nav.Link>
-            <Nav.Link
-              id={ComponentIDs.projectsMenuItem}
-              active={pathname === '/projects'}
-              href="/projects"
-              key="projects"
-            >
-              Projects
-            </Nav.Link>
-            <Nav.Link
-              id={ComponentIDs.interestsMenuItem}
-              active={pathname === '/interests'}
-              href="/interests"
-              key="interests"
-            >
-              Interests
-            </Nav.Link>
-            {currentUser
-              ? [
-                  <Nav.Link
-                    id={ComponentIDs.addProjectMenuItem}
-                    active={pathname === '/addProject'}
-                    href="/addProject"
-                    key="addP"
-                  >
-                    Add Project
-                  </Nav.Link>,
-                  <Nav.Link
-                    id={ComponentIDs.filterMenuItem}
-                    active={pathname === '/filter'}
-                    href="/filter"
-                    key="filter"
-                  >
-                    Filter
-                  </Nav.Link>,
-                ]
-              : ''}
+            {currentUser && role === 'ADMIN' && (
+              <Nav.Link
+                id="admin-search-nav"
+                active={pathname === '/adminSearch'}
+                href="/adminSearch"
+                key="adminSearch"
+              >
+                Admin Search
+              </Nav.Link>
+            )}
+            {currentUser && role === 'ADMIN' ? (
+              <Nav.Link id="admin-add-nav" href="/addRIO" key="admin" active={pathname === '/addRIO'}>
+                Add RIO
+              </Nav.Link>
+            ) : (
+              ''
+            )}
+            {currentUser && role === 'ADMIN-CLUB' && (
+              <Nav.Link
+                id="admin-club-edit-nav"
+                active={pathname === '/edit'}
+                href="/edit"
+                key="edit"
+              >
+                editRIO
+              </Nav.Link>
+            )}
           </Nav>
           <Nav className="justify-content-end">
             {currentUser ? (
@@ -89,6 +77,9 @@ const NavBar: React.FC = () => {
                 <NavDropdown.Item id={ComponentIDs.currentUserDropdownSignOut} href="/auth/signout">
                   <BoxArrowRight />
                   Sign out
+                </NavDropdown.Item>
+                <NavDropdown.Item id="editProfile" href="/auth/editProfile">
+                  Edit Profile
                 </NavDropdown.Item>
               </NavDropdown>
             ) : (
