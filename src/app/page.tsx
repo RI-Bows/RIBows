706fca7030c3ad6e874846ce6c024712f77213ff
Kate Hamada <kateh2@hawaii.edu>
2025-11-16 22:36:26 -1000

    Updated text for Hero Page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,23 +5,28 @@ export default function Home() {
   return (
     <main>
       <div id={PageIDs.landingPage}>
-        <div className="landing-green-background">
+        <div className="landing-white-background">
           <Container className="text-center">
             <h1
-              style={{ paddingTop: '20px', color: 'white', fontSize: '36pt' }}
+              style={{ paddingTop: '20px', color: 'black', fontSize: '36pt' }}
             >
-              Welcome to Bowfolios
+              Welcome to RIBows!
             </h1>
-            <h3 style={{ paddingBottom: '20px', color: 'white' }}>
-              Profiles, projects, and interest areas for the UH Community
+            <h3 style={{ paddingBottom: '20px', color: 'black' }}>
+              Where you can find/join clubs or Registered Independent Organizations (RIO&apos;s)
+              at UH Manoa
             </h3>
           </Container>
         </div>
         <div className="landing-white-background">
           <Container className="justify-content-center text-center">
-            <h2 style={{ color: '#376551' }}>
-              Start by making your profile....
+            <h2 style={{ color: 'black' }}>
+              Start by making your profile
             </h2>
+            <h5 style={{ color: 'black' }}>
+              Either by clicking sign in at the top right or clicking
+              <a href="/auth/signup" style={{ marginLeft: '4px' }}>here</a>
+            </h5>
             <Row md={1} lg={2}>
               <Col xs={6}>
                 <Image src="/images/home-page.png" width={500} alt="homepage" />
@@ -38,7 +43,9 @@ export default function Home() {
         </div>
         <div className="landing-green-background">
           <Container className="justify-content-center text-center">
-            <h2 style={{ color: 'white' }}>...then add your projects</h2>
+            <h3 style={{ paddingTop: '20px', color: 'white' }}>
+              Where you will then add your interests to your profile
+            </h3>
             <Row md={1} lg={2}>
               <Col xs={6}>
                 <Image
@@ -58,10 +65,15 @@ export default function Home() {
           </Container>
         </div>
         <div className="landing-white-background text-center">
-          <h2 style={{ color: '#376551' }}>
-            Connect to people and projects with shared interests!
-          </h2>
           <Container>
+            <h2 style={{ paddingTop: '20px', color: '#376551' }}>
+              Once you&apos;re all set up
+            </h2>
+            <h3 style={{ color: '#376551' }}>
+              Explore RIO&apos;s with the search tab that match your interests!
+              Bookmark them to join later or contact them directly through the app.
+              Or look at our most trending tab to see what&apos;s popular!
+            </h3>
             <Row md={1} lg={2}>
               <Col xs={6}>
                 <Image
