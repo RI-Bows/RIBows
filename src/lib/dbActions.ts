f08fa3ba72df3f2ba84cb4db0d033c722cd51c0a
ethnchiu <chiue2023@gmail.com>
2025-11-28 22:11:09 -1000

    Comment out template functions

--- src/lib/dbActions.ts
+++ src/lib/dbActions.ts
@@ -33,8 +33,6 @@ export async function changePassword(credentials: { email: string; password: str
   });
 }
 
-// TODO: Delete this eslint override once DB updated
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
 export async function createUser(credentials: { email: string; password: string }, interests: Interest[]) {
   // console.log(`createUser data: ${JSON.stringify(credentials, null, 2)}`);
   const password = await hash(credentials.password, 10);
@@ -52,21 +50,21 @@ export async function createUser(credentials: { email: string; password: string 
   });
 }
 
-export async function createProject(project: any) {
-  // console.log(`createProject data: ${JSON.stringify(project, null, 2)}`);
-  const dbProject = await prisma.project.create({
-    data: project,
-  });
-  return dbProject;
-}
+// export async function createProject(project: any) {
+//   // console.log(`createProject data: ${JSON.stringify(project, null, 2)}`);
+//   const dbProject = await prisma.project.create({
+//     data: project,
+//   });
+//   return dbProject;
+// }
 
-export type TrendingRio = {
-  id: number;
-  name: string;
-  blurb: string;
-  count: number;
-  interest: string;
-};
+// export type TrendingRio = {
+//   id: number;
+//   name: string;
+//   blurb: string;
+//   count: number;
+//   interest: string;
+// };
 
 // EC 11/24/25 - Commenting this out for now since schema was just updated and
 // popularity should be determined by something like number of bookmarks
@@ -160,109 +158,109 @@ export async function getInterests(): Promise<Interest[]> {
   return prisma.interest.findMany();
 }
 
-export async function upsertProject(project: any) {
-  // console.log(`upsertProject data: ${JSON.stringify(project, null, 2)}`);
-  const dbProject = await prisma.project.upsert({
-    where: { name: project.name },
-    update: {},
-    create: {
-      name: project.name,
-      description: project.description,
-      homepage: project.homepage,
-      picture: project.picture,
-    },
-  });
-  project.interests.forEach(async (interest: string) => {
-    const dbInterest = await prisma.interest.findUnique({
-      where: { name: interest },
-    });
-    // console.log(`${dbProject.name} ${dbInterest!.name}`);
-    const dbProjectInterest = await prisma.projectInterest.findMany({
-      where: { projectId: dbProject.id, interestId: dbInterest!.id },
-    });
-    if (dbProjectInterest.length === 0) {
-      await prisma.projectInterest.create({
-        data: {
-          projectId: dbProject.id,
-          interestId: dbInterest!.id,
-        },
-      });
-    }
-  });
-  project.participants.forEach(async (email: string) => {
-    const dbProfile = await prisma.profile.findUnique({
-      where: { email },
-    });
-    const dbProfileProject = await prisma.profileProject.findMany({
-      where: { projectId: dbProject.id, profileId: dbProfile!.id },
-    });
-    if (dbProfileProject.length === 0) {
-      await prisma.profileProject.create({
-        data: {
-          projectId: dbProject.id,
-          profileId: dbProfile!.id,
-        },
-      });
-    }
-  });
-  return dbProject;
-}
+// export async function upsertProject(project: any) {
+//   // console.log(`upsertProject data: ${JSON.stringify(project, null, 2)}`);
+//   const dbProject = await prisma.project.upsert({
+//     where: { name: project.name },
+//     update: {},
+//     create: {
+//       name: project.name,
+//       description: project.description,
+//       homepage: project.homepage,
+//       picture: project.picture,
+//     },
+//   });
+//   project.interests.forEach(async (interest: string) => {
+//     const dbInterest = await prisma.interest.findUnique({
+//       where: { name: interest },
+//     });
+//     // console.log(`${dbProject.name} ${dbInterest!.name}`);
+//     const dbProjectInterest = await prisma.projectInterest.findMany({
+//       where: { projectId: dbProject.id, interestId: dbInterest!.id },
+//     });
+//     if (dbProjectInterest.length === 0) {
+//       await prisma.projectInterest.create({
+//         data: {
+//           projectId: dbProject.id,
+//           interestId: dbInterest!.id,
+//         },
+//       });
+//     }
+//   });
+//   project.participants.forEach(async (email: string) => {
+//     const dbProfile = await prisma.profile.findUnique({
+//       where: { email },
+//     });
+//     const dbProfileProject = await prisma.profileProject.findMany({
+//       where: { projectId: dbProject.id, profileId: dbProfile!.id },
+//     });
+//     if (dbProfileProject.length === 0) {
+//       await prisma.profileProject.create({
+//         data: {
+//           projectId: dbProject.id,
+//           profileId: dbProfile!.id,
+//         },
+//       });
+//     }
+//   });
+//   return dbProject;
+// }
 
-export async function updateProfile(profile: any) {
-  console.log(`updateProfile data: ${JSON.stringify(profile, null, 2)}`);
-  const dbProfile = await prisma.profile.upsert({
-    where: { email: profile.email },
-    update: {
-      firstName: profile.firstName,
-      lastName: profile.lastName,
-      bio: profile.bio,
-    },
-    create: {
-      firstName: profile.firstName,
-      lastName: profile.lastName,
-      bio: profile.bio,
-      email: profile.email,
-    },
-  });
-  if (profile.interests) {
-    // Delete all profile interests
-    await prisma.profileInterest.deleteMany({
-      where: { profileId: dbProfile.id },
-    });
-    // Add the new profile interests
-    profile.interests.forEach(async (intere: string) => {
-      const dbInterest = await prisma.interest.findUnique({
-        where: { name: intere },
-      });
-      await prisma.profileInterest.create({
-        data: {
-          profileId: dbProfile.id,
-          interestId: dbInterest!.id,
-        },
-      });
-    });
-  }
-  if (profile.projects) {
-    // Delete all profile projects
-    await prisma.profileProject.deleteMany({
-      where: { profileId: dbProfile.id },
-    });
-    // Delete all the profile projects
-    await prisma.profileProject.deleteMany({
-      where: { profileId: dbProfile.id },
-    });
-    // Add the new profile projects
-    profile.projects.forEach(async (projectName: string) => {
-      const dbProject = await prisma.project.findUnique({
-        where: { name: projectName },
-      });
-      await prisma.profileProject.create({
-        data: {
-          profileId: dbProfile.id,
-          projectId: dbProject!.id,
-        },
-      });
-    });
-  }
-  return dbProfile;
-}
+// export async function updateProfile(profile: any) {
+//   console.log(`updateProfile data: ${JSON.stringify(profile, null, 2)}`);
+//   const dbProfile = await prisma.profile.upsert({
+//     where: { email: profile.email },
+//     update: {
+//       firstName: profile.firstName,
+//       lastName: profile.lastName,
+//       bio: profile.bio,
+//     },
+//     create: {
+//       firstName: profile.firstName,
+//       lastName: profile.lastName,
+//       bio: profile.bio,
+//       email: profile.email,
+//     },
+//   });
+//   if (profile.interests) {
+//     // Delete all profile interests
+//     await prisma.profileInterest.deleteMany({
+//       where: { profileId: dbProfile.id },
+//     });
+//     // Add the new profile interests
+//     profile.interests.forEach(async (intere: string) => {
+//       const dbInterest = await prisma.interest.findUnique({
+//         where: { name: intere },
+//       });
+//       await prisma.profileInterest.create({
+//         data: {
+//           profileId: dbProfile.id,
+//           interestId: dbInterest!.id,
+//         },
+//       });
+//     });
+//   }
+//   if (profile.projects) {
+//     // Delete all profile projects
+//     await prisma.profileProject.deleteMany({
+//       where: { profileId: dbProfile.id },
+//     });
+//     // Delete all the profile projects
+//     await prisma.profileProject.deleteMany({
+//       where: { profileId: dbProfile.id },
+//     });
+//     // Add the new profile projects
+//     profile.projects.forEach(async (projectName: string) => {
+//       const dbProject = await prisma.project.findUnique({
+//         where: { name: projectName },
+//       });
+//       await prisma.profileProject.create({
+//         data: {
+//           profileId: dbProfile.id,
+//           projectId: dbProject!.id,
+//         },
+//       });
+//     });
+//   }
+//   return dbProfile;
+// }
