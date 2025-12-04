e756bcad4e88275416b0d297721b7d5ed9f291e7
ethnchiu <chiue2023@gmail.com>
2025-12-03 17:53:33 -1000

    Restyle home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,4 @@
-import { Container, Row } from 'react-bootstrap';
+import { Container, Row, Col } from 'react-bootstrap';
 import { PageIDs } from '@/utilities/ids';
 import RioCardDisplay from '@/components/RioCardDisplay';
 import { getTrendingRios, RioType } from '@/lib/dbActions';
@@ -7,17 +7,52 @@ export default async function Home() {
   const rios: RioType[] = await getTrendingRios(9);
 
   return (
+    <Container fluid className="g-0">
+      <div className="home-background-image g-0">
+        <Row className="d-flex text-light text-center h-100 align-items-center">
+          <Col className="justify-content-center">
+            <h1 className="fw-bold py-3">
+              Find and join RIO&apos;s
+              <br />
+              at UH Mānoa
+            </h1>
+            <h2 className="py-3">
+              Use the search tab to
+              <br />
+              search by category or
+              <br />
+              keywords, or browse below
+            </h2>
+          </Col>
+        </Row>
+      </div>
+      <Row className="bg-light p-4 g-0">
+        <Container className="pt-3 w-75">
+          <h2 className="text-primary fw-bold text-start pb-4">
+            Trending RIOs
+          </h2>
+          <div className="trending-panel">
+            <Row xs={1} md={3} className="g-4 py-1">
+              <RioCardDisplay rioList={rios} />
+            </Row>
+          </div>
+        </Container>
+      </Row>
+    </Container>
+  );
+
+  return (
     <main>
       <div id={PageIDs.landingPage}>
         <div className="Hero-background-image">
           <section className="Hero-section">
             <Container className="text-center">
-              <h1 className="hero-simple-subtitle py-3">
+              <h1 className="text-light fw-bold py-3">
                 Find and join RIO&apos;s
                 <br />
                 at UH Mānoa
               </h1>
-              <h2 className="hero-simple-subtitle-under">
+              <h2 className="text-light py-3">
                 Use the search tab to
                 <br />
                 search by category or
