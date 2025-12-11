12196613cdebe7ca070fcdd59a3792b3ef04e584
Kate Hamada <kateh2@hawaii.edu>
2025-12-11 08:38:14 -1000

    Fix Timeout Issue

--- playwright.config.ts
+++ playwright.config.ts
@@ -12,6 +12,8 @@ import { defineConfig, devices } from '@playwright/test';
  */
 export default defineConfig({
   testDir: './tests',
+  /* Maximum time one test can run for. */
+  globalTimeout: 60000,
   /* Run tests in files in parallel */
   fullyParallel: true,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
@@ -72,7 +74,7 @@ export default defineConfig({
   /* Run your local dev server before starting the tests */
   webServer: {
     command: 'npm run dev',
-    //url: 'http://127.0.0.1:3000',
+    // url: 'http://127.0.0.1:3000',
     reuseExistingServer: !process.env.CI,
   },
 });
